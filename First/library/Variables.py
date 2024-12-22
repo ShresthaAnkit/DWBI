9d67cd68a45f0f315178197949a92c1fd80c0398
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2024-12-22 11:07:03 +0545

    Fix path issue

--- First/library/Variables.py
+++ First/library/Variables.py
@@ -2,7 +2,7 @@ import json
 
 class Variables:
     def __init__(self,variable_name):
-        self.path = r"D:\Programming\Python\College\config\config.cfg"
+        self.path = r"D:\Programming\Python\College\First\config\config.cfg"
         self.name = variable_name
 
     def get_variable(self):

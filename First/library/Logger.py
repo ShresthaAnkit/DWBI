86172a3b4f58d14a1fedeb7b03b396e22156909e
AnkitShrestha-Xenserv <aankitshrestha@gmail.com>
2024-12-29 08:57:23 +0545

    implement logger class

--- First/library/Logger.py
+++ First/library/Logger.py
@@ -1,13 +1,26 @@
 # Keep track of every query
 import logging
-
+from Variables import Variables
 import datetime
-from os import path
+import os
 
 class Logger:
-    def log_initialize():
-        pass
+    def __init__(self,file_name):
+        current_ts = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+        self.file_name = f"{file_name}_{current_ts}"
+        self.log_path = os.path.join(Variables.get_variable('log_path'),self.file_name)
 
-    def log_message(msg):
-        msg = msg
-        log_file.write(str)
+        self.logger = self.set_logger()
+
+    def set_logger(self):
+        self.logger = logging.getLogger()
+        self.logger.setLevel('INFO')
+        return self.logger
+
+    def log_info(self,msg):
+        self.logger.info(msg)
+    def log_error(self,msg):
+        self.logger.error(msg)
+        
+
+log = Logger('test')
